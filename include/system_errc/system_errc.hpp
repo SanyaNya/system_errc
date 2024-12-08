360b34636ac75c851e07603f2485978f7123137d
SanyaNya <sanyanya4ever@gmail.com>
2024-12-08 21:51:24 +0300

    Basic ErrorCode struct and get set error

--- include/system_errc/system_errc.hpp
+++ include/system_errc/system_errc.hpp
@@ -1,1 +1,52 @@
 #pragma once
+
+#if defined(WIN32) || defined(__MINGW32__)
+  #define SYSERRC_WIN_IMPL 1
+  #define SYSERRC_POSIX_IMPL 0
+#else
+  #define SYSERRC_WIN_IMPL 0
+  #define SYSERRC_POSIX_IMPL 1
+#endif
+
+#if SYSERRC_WIN_IMPL
+  #include <errhandlingapi.h>
+#elif SYSERRC_POSIX_IMPL
+  #include <cerrno>
+#endif
+
+namespace sys_errc
+{
+
+using ErrType =
+#if SYSERRC_WIN_IMPL
+  decltype(GetLastError());
+#elif SYSERRC_POSIX_IMPL
+  decltype(errno);
+#endif
+
+struct ErrorCode
+{
+  explicit constexpr ErrorCode(ErrType v) noexcept : m_value_(v) {}
+
+  ErrType m_value_;
+};
+
+[[nodiscard]] inline ErrorCode last_error() noexcept
+{
+#if SYSERRC_WIN_IMPL
+  return ErrorCode(GetLastError());
+#elif SYSERRC_POSIX_IMPL
+  return ErrorCode(errno);
+#endif
+}
+
+inline void set_error(ErrType e) noexcept
+{
+#if SYSERRC_WIN_IMPL
+  SetLastError(e);
+#elif SYSERRC_POSIX_IMPL
+  errno = e;
+#endif
+}
+
+} //namespace sys_errc
