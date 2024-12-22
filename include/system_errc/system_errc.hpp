4c7baa5c71d11863ffcb4c241ef83bb2c0cc9f60
SanyaNya <sanyanya4ever@gmail.com>
2024-12-22 12:53:11 +0300

    Simplify message() api and take into account gnu and posix variants of strerror_r

--- include/system_errc/system_errc.hpp
+++ include/system_errc/system_errc.hpp
@@ -14,8 +14,9 @@
   #include <cerrno>
 #endif
 
-#include <cstdint>
+#include <cstddef>
 #include <cstring>
+#include <type_traits>
 
 namespace sys_errc
 {
@@ -27,24 +28,12 @@ using ErrType =
   decltype(errno);
 #endif
 
-template<typename CharT, std::size_t Size>
-struct cstring
-{
-  constexpr CharT* data() noexcept { return m_str_; }
-  constexpr const CharT* data() const noexcept { return m_str_; }
-
-  constexpr std::size_t capacity() const noexcept { return Size; }
-
-private:
-  CharT m_str_[Size];
-};
-
 struct ErrorCode
 {
 #if SYSERRC_WIN_IMPL
-  using msg_type = cstring<wchar_t, 256>;
+  using char_type = wchar_t;
 #elif SYSERRC_POSIX_IMPL
-  using msg_type = cstring<char, 256>;
+  using char_type = char;
 #endif
 
   explicit constexpr ErrorCode(ErrType v) noexcept : m_value_(v) {}
@@ -52,15 +41,17 @@ struct ErrorCode
   constexpr bool operator==(ErrorCode ec) const noexcept { return m_value_ == ec.m_value_; }
   constexpr bool operator!=(ErrorCode ec) const noexcept { return m_value_ != ec.m_value_; }
 
-  msg_type message() const noexcept
+  const char_type* message() const noexcept
   {
-    msg_type msg;
+    static thread_local char_type msg[256];
 
   #if SYSERRC_WIN_IMPL
 	  const int f = FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK;
-	  FormatMessageW(f, 0, m_value_, 0, msg.data(), msg.capacity(), 0);
+	  FormatMessageW(f, 0, m_value_, 0, msg, sizeof(msg), 0);
   #elif SYSERRC_POSIX_IMPL
-	  strerror_r(m_value_, msg.data(), msg.capacity());
+	  auto r = strerror_r(m_value_, msg, sizeof(msg));
+    if constexpr(std::is_same_v<decltype(r), char*>) //GNU variant
+      return r;
   #endif
 
     return msg;
